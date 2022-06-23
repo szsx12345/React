8c1a0172aa92f5745b7af00fbfaef12040bd90d2
szsx12345 <szsx12345@hotmail.com.tw>
2022-06-23 11:42:32 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const isDev = require('electron-is-dev');
+
+function createWindow () {
+    const mainWindow = new BrowserWindow({
+        frame: true,
+        kiosk: false,
+        width: 1920,
+        height: 1080,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    if (isDev) {
+        // 開發階段直接與 React 連線
+        mainWindow.loadURL('http://localhost:3000/');
+        // 開啟 DevTools.
+        //mainWindow.webContents.openDevTools()
+    } else {
+        // 產品階段直接讀取 React 打包好的
+        mainWindow.loadFile('./build/index.html');
+    }
+}
+
+app.whenReady().then(() => {
+    createWindow()
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
